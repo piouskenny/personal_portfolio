5bb5d69a5f01ab254b9e2dd0aacfca7e6988f97b
piouskenny <kehindeu13@gmail.com>
2023-04-07 10:25:41 +0100

    updated the portfolio

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -35,7 +35,7 @@ const Portfolio = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <button className="mt-20 rounded-full backdrop-blur-sm bg-white/10 text-white px-10 py-2 shadow-md shadow-white/10  transition md:hover:scale-150">
+            <button className="md:mt-20 mt-4 rounded-full backdrop-blur-sm bg-white/10 text-white px-10 py-2 shadow-md shadow-white/10  transition md:hover:scale-150">
               My GitHub &rarr;
             </button>
           </a>
